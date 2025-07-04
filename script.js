b7881cbdbf73b90256f33839d9542c8c94a29d2c
sneha <snehailager76@gmail.com>
2025-07-04 14:11:10 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function hello(){
+                alert("hello, this is an internal javascript demo that is written within the body tag of the html");
+                console.log("this will be printed on the console");
+                console.warn("this is a warning message");
+                console.error("this is an error message");
+                console.info("this is an info message");
+                console.debug("this is a debug message");
+                console.log("this is a log message");
+            }
